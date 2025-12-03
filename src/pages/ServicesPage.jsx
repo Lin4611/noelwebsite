a0faeb985a299e5e84eaac9587740edbf133b616
Lin <tzu.hsun.lin046@gmail.com>
2025-12-03 09:51:32 +0800

    feat: complete price list and RWD

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -4,6 +4,9 @@ import ui_icon from "../assets/imgs/icon/service-item-ui.svg";
 import html_css_icon from "../assets/imgs/icon/service-item-html&css.svg";
 import front_end_icon from "../assets/imgs/icon/service-item-front-end.svg";
 import arrow_down_icon from "../assets/imgs/icon/arrow-downward.svg";
+import price_visual_icon from "../assets/imgs/icon/price-item-visual.svg";
+import pirce_ui_icon from "../assets/imgs/icon/price-item-ui.svg";
+import price_frontend_icon from "../assets/imgs/icon/price-item-front-end.svg";
 const servicesArray = [
   { id: 1, title: "平面設計", iconUrl: visual_design_icon },
   { id: 2, title: "UI 設計", iconUrl: ui_icon },
@@ -37,8 +40,14 @@ const processArray = [
   { id: 6, title: "終版確認", content: "雙方同意後即為最終版" },
   { id: 7, title: "結案", content: "客戶簽署驗收單，並於約定日期支付尾款" },
 ];
+const priceArray = [
+  { id: 1, iconUrl: price_visual_icon, title: "平面設計", price: "6,000" },
+  { id: 2, iconUrl: pirce_ui_icon, title: "UI設計", price: "15,000" },
+  { id: 3, iconUrl: price_frontend_icon, title: "前端開發", price: "22,000" },
+];
 const services_bg = "bg-[url('../assets/imgs/service-bg/service-item-bg.png')]";
-const process_bg = "bg-[url('../assets/imgs/service-bg/process-bg.png')] bg-[length:100%_100%]";
+const process_bg =
+  "bg-[url('../assets/imgs/service-bg/process-bg.png')] bg-[length:100%_100%]";
 const ServicesPage = () => {
   return (
     <>
@@ -88,6 +97,31 @@ const ServicesPage = () => {
                   />
                 )}
               </>
+            ))}
+          </div>
+        </section>
+        <section className="w-full flex flex-col gap-10 py-20 items-center justify-center">
+          <SectionTitle title={"價格表"} />
+          <div className="w-full grid grid-cols-1 gap-6 place-items-center px-[27px] lg:grid-cols-2 lg:max-w-[1296px] xl:grid-cols-3">
+            {priceArray.map((priceItem) => (
+              <div
+                key={priceItem.id}
+                className="w-full py-6 px-6 flex justify-between items-center bg-Primary-100 shadow-[0_4px_8px_rgba(0,0,0,0.08)] rounded-lg max-w-[416px]"
+              >
+                <div className="flex items-center gap-3">
+                  <img
+                    src={priceItem.iconUrl}
+                    alt={priceItem.title}
+                    className="w-10 h-10"
+                  />
+                  <h3 className="h5 text-Primary-800">{priceItem.title}</h3>
+                </div>
+                <div className="max-w-[41px] w-full border border-Primary-400"></div>
+                <div className="flex items-baseline gap-1">
+                  <h3 className="h4-bold text-Primary-800">{priceItem.price}</h3>
+                  <p className="fs-6 text-Primary-600">起</p>
+                </div>
+              </div>
             ))}
           </div>
         </section>
